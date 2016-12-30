978d4f02f517941e7a62ef6534367dc7c3755ddd
cheyulin <yche@cse.ust.hk>
2016-12-30 19:41:50 +0800

    refactor exp matrix

--- 2014-Heat-Kernel/python/gexpmq.py
+++ 2014-Heat-Kernel/python/gexpmq.py
@@ -2,9 +2,7 @@
 demo_gexpmq.py
 
 A demonstration of the Gauss-Seidel coordinate relaxation scheme
-to estimate a column of the matrix exponential that operates
-only on significant entries.
-
+to estimate a column of the matrix exponential that operates only on significant entries.
 Written by Kyle Kloster and David F. Gleich
 """
 
@@ -29,6 +27,7 @@ if __name__ == '__main__':
     c = 1  # the column to compute
     psis = compute_psis(N)
     threshs = compute_threshs(eps, N, psis)
+
     # Initialize variables
     x = {}  # Store x, r as dictionaries
     r = {}  # initialize residual
@@ -37,6 +36,7 @@ if __name__ == '__main__':
     r[(c, 0)] = 1.
     Q.append(c)
     sumresid += psis[0]
+
     # Main loop
     for j in xrange(0, N):
         qsize = len(Q)
@@ -46,16 +46,20 @@ if __name__ == '__main__':
             rij = r[(i, j)]
             if rij < relaxtol:
                 continue
+
             # perform the relax step
-            if i not in x: x[i] = 0.
+            if i not in x:
+                x[i] = 0.
             x[i] += rij
             r[(i, j)] = 0.
             sumresid -= rij * psis[j]
             update = (rij / (float(j) + 1.)) / len(G[i])
+
             for u in G[i]:  # for neighbors of i
                 next = (u, j + 1)
                 if j == N - 1:
-                    if u not in x: x[u] = 0.
+                    if u not in x:
+                        x[u] = 0.
                     x[u] += update
                 else:
                     if next not in r:
@@ -64,9 +68,12 @@ if __name__ == '__main__':
                     r[next] += update
                     sumresid += update * psis[j + 1]
             # after all neighbors u
-            if sumresid < eps: break
-        if len(Q) == 0: break
-        if sumresid < eps: break
+            if sumresid < eps:
+                break
+        if len(Q) == 0:
+            break
+        if sumresid < eps:
+            break
 
     for v in xrange(1, len(G) + 1):
         if v in x:
