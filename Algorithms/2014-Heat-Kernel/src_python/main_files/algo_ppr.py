ff297c5357d764b07e1315fc725d692a6d25ac8e
cheyulin <yche@cse.ust.hk>
2017-01-18 18:56:05 +0800

    refactor to make it readable

--- Algorithms/2014-Heat-Kernel/src_python/main_files/algo_ppr.py
+++ Algorithms/2014-Heat-Kernel/src_python/main_files/algo_ppr.py
@@ -1,18 +1,19 @@
 import collections
+import random
 
 from util_helper import *
 
 
 class PPR:
-    def __init__(self, graph, alpha, tol):
+    def __init__(self, graph, alpha, tolerance):
         self.graph = graph
         self.alpha = alpha
-        self.tol = tol
+        self.tolerance = tolerance
         self.volG = get_edge_num(graph)
 
-    @staticmethod
-    def generate_seed_list():
-        return [1]
+    def generate_seed_list(self):
+        rand_int = random.randint(1, len(self.graph))
+        return [rand_int]
 
     def estimate_ppr_vector(self, seed_list):
         x_dict, r_dict = {}, {}
@@ -23,7 +24,7 @@ class PPR:
             task_queue.append(vertex)
 
         while len(task_queue) > 0:
-            v = task_queue.popleft()  # v has r[v] > tol*deg(v)
+            v = task_queue.popleft()
             if v not in x_dict:
                 x_dict[v] = 0.
             x_dict[v] += (1 - self.alpha) * r_dict[v]
@@ -32,46 +33,39 @@ class PPR:
             for neighbor_v in self.graph[v]:
                 if neighbor_v not in r_dict:
                     r_dict[neighbor_v] = 0.
-                if r_dict[neighbor_v] < len(self.graph[neighbor_v]) * self.tol <= r_dict[neighbor_v] + mass:
+                if r_dict[neighbor_v] < len(self.graph[neighbor_v]) * self.tolerance <= r_dict[neighbor_v] + mass:
                     task_queue.append(neighbor_v)
                 r_dict[neighbor_v] += mass
             r_dict[v] = mass * len(self.graph[v])
-            if r_dict[v] >= len(self.graph[v]) * self.tol:
+            if r_dict[v] >= len(self.graph[v]) * self.tolerance:
                 task_queue.append(v)
         print str(x_dict)
         return x_dict
 
     def sweep_cut(self, x_dict):
-        # Find cluster
-        for v in x_dict:
-            x_dict[v] /= len(self.graph[v])
-        sorted_pair = sorted(x_dict.iteritems(), key=lambda x: x[1], reverse=True)
-
+        sorted_pair = sorted(map(lambda ele: (ele[0], ele[1] / len(self.graph[ele[0]])), x_dict.iteritems()),
+                             key=lambda x: x[1], reverse=True)
         S = set()
         volS, cutS = 0., 0.
-        bestcond = 1.
-        bestset = sorted_pair[0]
+        best_cond = 1.
+        best_set = sorted_pair[0]
         for p in sorted_pair:
             vertex = p[0]  # get the vertex
             volS += len(self.graph[vertex])  # add degree to volume
-            for v in self.graph[vertex]:
-                if v in S:
-                    cutS -= 1
-                else:
-                    cutS += 1
-            print "v: %4i  cut: %4f  vol: %4f" % (vertex, cutS, volS)
+            cutS += sum(map(lambda v: -1 if v in S else 1, self.graph[vertex]))
             S.add(vertex)
-            if cutS / min(volS, self.volG - volS) < bestcond:
-                bestcond = cutS / min(volS, self.volG - volS)
-                bestset = set(S)  # make a copy
-        print "Best set conductance: %f" % (bestcond)
-        print "  set = ", str(bestset)
+            if cutS / min(volS, self.volG - volS) < best_cond:
+                best_cond = cutS / min(volS, self.volG - volS)
+                best_set = set(S)  # make a copy
+        return best_cond, best_set
 
     def do_iterations(self):
-        x_dict = self.estimate_ppr_vector(PPR.generate_seed_list())
-        self.sweep_cut(x_dict)
+        x_dict = self.estimate_ppr_vector(self.generate_seed_list())
+        best_cond, best_set = self.sweep_cut(x_dict)
+        print "Best set conductance: %f" % (best_cond)
+        print "  set = ", str(best_set)
 
 
 if __name__ == '__main__':
     adj_list_dict = get_sample_graph()
-    PPR(graph=adj_list_dict, alpha=0.99, tol=0.01).do_iterations()
+    PPR(graph=adj_list_dict, alpha=0.99, tolerance=0.01).do_iterations()
