ceb4efaa29c9903c9060ebd6b63c05dddc0cc1e6
cheyulin <yche@cse.ust.hk>
2017-03-22 15:18:17 +0800

    add some graph format samples

--- Prensentation/graph_serialization/graph_formats.py
+++ Prensentation/graph_serialization/graph_formats.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from networkx.readwrite import json_graph
+
 import json
 
 if __name__ == '__main__':
@@ -7,16 +8,27 @@ if __name__ == '__main__':
     assert isinstance(graph, nx.Graph)
     print 'edges:', graph.edges()
 
-    nx.write_adjlist(graph, 'concept_output/example_graph.adjlist')
-    nx.write_multiline_adjlist(graph, 'concept_output/example_graph.multiline_adjlist')
-    nx.write_edgelist(graph, 'concept_output/example_graph.edgelist')
+    # raw
+    nx.write_adjlist(graph, 'output_raw/example_graph.adjlist')
+    nx.write_multiline_adjlist(graph, 'output_raw/example_graph.multiline_adjlist')
+    nx.write_edgelist(graph, 'output_raw/example_graph.edgelist')
 
-    nx.write_gpickle(graph, 'bin_output/example_graph.pickle')
+    # better serialization
+    nx.write_gpickle(graph, 'output_serialization/example_graph.pickle')
+    nx.write_yaml(graph, 'output_serialization/example_graph.yaml')
+    nx.write_graph6(graph, 'output_serialization/example_graph.graph6')
 
-    nx.write_gexf(graph, 'xml_output/example_graph.gexf')
-    nx.write_graphml(graph, 'xml_output/example_graph.graphml')
+    # xml
+    nx.write_gexf(graph, 'output_xml/example_graph.gexf')
+    nx.write_graphml(graph, 'output_xml/example_graph.graphml')
 
-    nx.write_gml(graph, 'example_graph.gml')
+    # json
+    with open('output_json/node_link.json', 'w') as outfile:
+        json.dump(json_graph.node_link_data(graph), outfile, indent=2)
 
-    with open('json_output/node_link.json', 'w') as outfile:
-        json.dump(json_graph.node_link_data(graph), outfile, indent=4)
+    with open('output_json/adjacency.json', 'w') as outfile:
+        json.dump(json_graph.adjacency_data(graph), outfile, indent=2)
+
+    # other
+    nx.write_gml(graph, 'output_other/example_graph.gml')
+    nx.write_pajek(graph, 'output_other/example_graph.pajek')
---
+++ Prensentation/graph_serialization/output_other/example_graph.gml
@@ -0,0 +1,38 @@
+graph [
+  node [
+    id 0
+    label 2
+  ]
+  node [
+    id 1
+    label 3
+  ]
+  node [
+    id 2
+    label 4
+  ]
+  node [
+    id 3
+    label 7
+  ]
+  edge [
+    source 0
+    target 1
+    weight 4.0
+  ]
+  edge [
+    source 0
+    target 3
+    weight 4.0
+  ]
+  edge [
+    source 1
+    target 2
+    weight 4.0
+  ]
+  edge [
+    source 2
+    target 3
+    weight 4.0
+  ]
+]
